67e3bd2cca4c738a253db8b6842f77b114232c9e
Georgi H <34049724+cryptozealot@users.noreply.github.com>
2019-07-15 16:58:20 +0300

    Update hammer.sh

--- hammer.sh
+++ hammer.sh
@@ -1,15 +1,16 @@
-!/bin/bash
+#!/bin/bash
 
-#Usage ./hammer {total requests to make} {delay between requests in s} {endpoint}
-#Example ./hammer 50 0.5 http://ratings/api/rate/RD-10/3
+#Usage ./hammer {total requests to make} {delay between requests in s} {endpoint} {method - PUT,GET, etc..}
+#Example ./hammer 50 0.5 http://ratings/api/rate/RD-10/3 PUT
 
 times=$1
 sleeptime=$2
 endpoint=$3
+method=$4
 i=0
 while [ $i -le $times ]
 do
         i=$(($i+1))
-        curl -X PUT $endpoint
+        curl -X $method $endpoint
         sleep $sleeptime
 done
