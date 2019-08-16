2e5f8015adea16c9ad75e79bb56aad067e985cb9
Georgi H <34049724+cryptozealot@users.noreply.github.com>
2019-08-16 12:57:31 +0300

    f1

--- k8sauto.sh
+++ k8sauto.sh
@@ -38,7 +38,7 @@ apt-get update
 swapoff -a
 
 #init k8s cluster
-kubeadm init
+kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
