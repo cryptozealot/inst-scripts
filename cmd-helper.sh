b48cda2bfb2e0bbc75cfa492c7516bf84032f6d8
Georgi H <34049724+cryptozealot@users.noreply.github.com>
2019-08-16 17:48:40 +0300

    automatic installation
    Script is now automated.
    Chmod it, run it, and then run "source ~/.bashrc"

--- cmd-helper.sh
+++ cmd-helper.sh
@@ -1,24 +1,32 @@
-########## put this in ~/.bashrc
+#!/bin/bash
+#chmod this script, execute it and source it to be able to use it
 
-source <(kubectl completion bash)
+echo "source <(kubectl completion bash)
 alias k='kubectl'
 complete -F __start_kubectl k
 alias kn='namespaceset'
+alias brc='source ~/.bashrc'"  >> ~/.bashrc
 
-########## k8s namespace selector script: /bin/namespaceset.sh
+echo "namespace=\$1
+kubectl config set-context --current --namespace=\$1
+kubectl config view | grep namespace"  >> /bin/namespaceset
 
-namespace=$1
-kubectl config set-context --current --namespace=$1
-kubectl config view | grep namespace
+chmod +x /bin/namespaceset
 
+#Source the changes in .bashrc using the commands below
 
-########## docker exec into container
+#source ~/.bashrc
+#source /root/.bashrc
+#brc
 
-container_name_or_id=$1
-docker exec -it $1 /bin/bash
+## Use commands below manually
+########### docker exec into container
+
+#container_name_or_id=$1
+#docker exec -it $1 /bin/bash
 
 ########## kubectl exec into container
 
-container=$1
-kubectl exec -it $1 -- /bin/bash
-kubectl exec -it $1 -- /bin/sh
+#container=$1
+#kubectl exec -it $1 -- /bin/bash
+#kubectl exec -it $1 -- /bin/sh
